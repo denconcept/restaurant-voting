85f9a418b01179ac63f6d76bc8b5bc5d3a96d3d3
Denis <coddden@gmail.com>
2026-06-04 15:22:38 +0500

    Add repository query methods

--- src/main/java/com/denconcept/restaurantvoting/vote/repository/VoteRepository.java
+++ src/main/java/com/denconcept/restaurantvoting/vote/repository/VoteRepository.java
@@ -3,5 +3,10 @@ package com.denconcept.restaurantvoting.vote.repository;
 import com.denconcept.restaurantvoting.vote.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
+
+    Optional<Vote> findByUserIdAndVoteDate(Integer userId, LocalDate voteDate);
 }
