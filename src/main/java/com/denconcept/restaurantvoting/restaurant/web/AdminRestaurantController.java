28fdd8200cece156dc402611fce58eaa4f0cb045
Denis <coddden@gmail.com>
2026-06-09 16:02:18 +0500

    Add restaurant management endpoints

--- src/main/java/com/denconcept/restaurantvoting/restaurant/web/AdminRestaurantController.java
+++ src/main/java/com/denconcept/restaurantvoting/restaurant/web/AdminRestaurantController.java
@@ -1,12 +1,55 @@
 package com.denconcept.restaurantvoting.restaurant.web;
 
+import com.denconcept.restaurantvoting.common.error.NotFoundException;
+import com.denconcept.restaurantvoting.restaurant.model.Restaurant;
+import com.denconcept.restaurantvoting.restaurant.repository.RestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = AdminRestaurantController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
 public class AdminRestaurantController {
 
     public static final String REST_URL = "/api/admin/restaurants";
+    private final RestaurantRepository restaurantRepository;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Restaurant> createWithLocation(@RequestBody Restaurant restaurant) {
+        Restaurant created = restaurantRepository.save(restaurant);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @GetMapping("/{id}")
+    public Restaurant get(@PathVariable Integer id) {
+        return restaurantRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Restaurant not found with id=" + id));
+    }
+
+    @GetMapping
+    public List<Restaurant> getAll() {
+        return restaurantRepository.findAll();
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody Restaurant restaurant) {
+        restaurantRepository.save(restaurant);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        restaurantRepository.deleteById(id);
+    }
 }
