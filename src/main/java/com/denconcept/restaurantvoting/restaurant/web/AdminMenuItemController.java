c8fe4d97f21b0f2a3b90ca9c1a59320341820132
Denis <coddden@gmail.com>
2026-06-09 17:50:33 +0500

    Add menu item management endpoints

--- src/main/java/com/denconcept/restaurantvoting/restaurant/web/AdminMenuItemController.java
+++ src/main/java/com/denconcept/restaurantvoting/restaurant/web/AdminMenuItemController.java
@@ -1,12 +1,56 @@
 package com.denconcept.restaurantvoting.restaurant.web;
 
+import com.denconcept.restaurantvoting.common.error.NotFoundException;
+import com.denconcept.restaurantvoting.restaurant.model.Menu;
+import com.denconcept.restaurantvoting.restaurant.model.MenuItem;
+import com.denconcept.restaurantvoting.restaurant.repository.MenuItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = AdminMenuItemController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
 public class AdminMenuItemController {
 
     public static final String REST_URL = "/api/admin/menu-items";
+    private final MenuItemRepository menuItemRepository;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MenuItem> createWithLocation(@RequestBody MenuItem menuItem) {
+        MenuItem created = menuItemRepository.save(menuItem);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @GetMapping("/{id}")
+    public MenuItem get(@PathVariable Integer id) {
+        return menuItemRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("MenuItem not found with id=" + id));
+    }
+
+    @GetMapping
+    public List<MenuItem> getAll() {
+        return menuItemRepository.findAll();
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody MenuItem menuItem) {
+        menuItemRepository.save(menuItem);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        menuItemRepository.deleteById(id);
+    }
 }
