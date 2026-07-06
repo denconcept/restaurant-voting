bdd02cea5bb031aa8010339015eea3790f1ee527
Denis <coddden@gmail.com>
2026-07-06 17:47:27 +0500

    Optimize menu loading with EntityGraph

--- src/main/java/com/denconcept/restaurantvoting/restaurant/repository/MenuRepository.java
+++ src/main/java/com/denconcept/restaurantvoting/restaurant/repository/MenuRepository.java
@@ -1,6 +1,7 @@
 package com.denconcept.restaurantvoting.restaurant.repository;
 
 import com.denconcept.restaurantvoting.restaurant.model.Menu;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -8,5 +9,6 @@ import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
 
+    @EntityGraph(attributePaths = {"restaurant", "menuItems"})
     List<Menu> findAllByMenuDate(LocalDate menuDate);
 }
