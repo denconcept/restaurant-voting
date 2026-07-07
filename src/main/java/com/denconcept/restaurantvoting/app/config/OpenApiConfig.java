20b3288670069f66cf0d9d4769a33ab20a4ef0b3
Denis <coddden@gmail.com>
2026-07-07 20:21:03 +0500

    Improve OpenAPI configuration

--- src/main/java/com/denconcept/restaurantvoting/app/config/OpenApiConfig.java
+++ src/main/java/com/denconcept/restaurantvoting/app/config/OpenApiConfig.java
@@ -2,8 +2,11 @@ package com.denconcept.restaurantvoting.app.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -13,7 +16,34 @@ import org.springframework.context.annotation.Configuration;
         scheme = "basic"
 )
 @OpenAPIDefinition(
+        info = @Info(
+                title = "Restaurant Voting REST API",
+                version = "1.0",
+                description = """
+                        REST API for voting system for deciding where to have lunch
+                        <p><b>Test credentials:</b><br>
+                        - admin@gmail.com / admin<br>
+                        - user@gmail.com / user</p>
+                        """
+        ),
         security = @SecurityRequirement(name = "basicAuth")
 )
 public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+                .group("Admin API")
+                .pathsToMatch("/api/admin/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+                .group("User API")
+                .pathsToMatch("/api/**")
+                .pathsToExclude("/api/admin/**")
+                .build();
+    }
 }
